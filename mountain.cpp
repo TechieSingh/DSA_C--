f4cc00809c5ff1700201b1069bc2dbf01c32d605
Aditya Ranjan Singh <37181972+TechieSingh@users.noreply.github.com>
2021-06-15 17:28:58 +0530

    update

--- mountain.cpp
+++ mountain.cpp
@@ -4,32 +4,25 @@ using namespace std;
 
 int findpeek(vector<int> arr){
     int n= arr.size();
-    int peek=0;
-    int height;
-    for (int i = 0; i <n-1; i++)
+    int largest;
+    for (int i = 1; i <n-2;)
     {
-        int j=i+1;
-        int k=j+1;
-        if(arr[i]<arr[j]&&arr[j]>arr[k]){
-            peek=arr[j];
-           cout<<peek;
-           height = findheight(arr,i,j,k); 
-
-        }  
+       
+        if(arr[i]>arr[i-1] and arr[i]>arr[i+1]){ 
+            cout<<arr[i];
+            i++;
+        }
+        else
+        i++ ; 
     }
     
     return 0;
 }
 
-int findheight(vector<int> arr,int i, int j, int k){
-    int sum;
-    int up;
-    int down;
-    for(int i=0;i<n-1)
-}
 
 int main(){
     vector<int> arr{5,6,1,2,3,4,5,4,3,2,0,1,2,3,-2,4};
     auto result = findpeek(arr);
+    cout<<result;
 
 }
